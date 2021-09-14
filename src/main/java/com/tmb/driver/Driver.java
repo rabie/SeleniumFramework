b2409430a40e14ec7361217fd6070bdc8064cdb9
Rabie El ouargua <rabie.elouargua@gmail.com>
2021-09-14 18:15:22 +0300

    Adding Driver and remove duplicates

--- src/main/java/com/tmb/driver/Driver.java
+++ src/main/java/com/tmb/driver/Driver.java
@@ -1,7 +1,6 @@
 package com.tmb.driver;
 
 import com.tmb.constants.FrameworkConstants;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.Objects;
--- src/main/java/com/tmb/driver/DriverManager.java
+++ src/main/java/com/tmb/driver/DriverManager.java
@@ -11,7 +11,6 @@ public final class DriverManager {
     public static WebDriver getDriver() {
         return driver.get();
     }
-
     public static void setDriver(WebDriver driverRef) {
         driver.set(driverRef);
     }
